79d94a14eb436ec08a29b9188f93857e480b1f35
JunCodera1 <minhtien0601200@gmail.com>
2024-09-09 00:16:07 +0700

    darkmode

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,11 +10,19 @@ const Hero = () => {
         <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='purple' />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] absolute flex items-center justify-center">
         {/* Radial gradient for the  container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
         
         
+      </div>
+
+      <div className="flex justify-center relative my-20">
+          <div className='max-w-[89vh] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+              <h2 className='uppercase tracking-widest text-xs text-center text-blue-100'>
+                Dynamic Web Magic with Next.js
+              </h2>
+          </div>
       </div>
     </div>
   )
