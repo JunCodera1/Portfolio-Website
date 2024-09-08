34478dace8b9085172f3bf337a03c7f467927b07
JunCodera1 <minhtien0601200@gmail.com>
2024-09-08 09:48:29 +0700

    rebuild

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="relative bg-black">
+        <h1>Hello, Portfolio</h1>
+      </div>
+    </main>
+  );
+}
