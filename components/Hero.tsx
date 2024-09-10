4d23fa4a8a221e109c5538810eb1f07c991bb544
JunCodera1 <minhtien0601200@gmail.com>
2024-09-10 09:13:41 +0700

    add Magic Button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -29,8 +31,16 @@ const Hero = () => {
               />
 
               <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-                  Hi, I&apos;m Jun, a Next.js Developer based in Viet Nam
+                  Hi, I&apos;m Jun, a Next.js Developer based in Viet Nam.
               </p>
+
+              <a href="#about">
+                <MagicButton
+                  title="Show my work"
+                  icon={<FaLocationArrow/>}
+                  position='right'
+                />
+              </a>
           </div>
       </div>
     </div>
