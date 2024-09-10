036853687118fe1888ae73e450431a993842570e
JunCodera1 <minhtien0601200@gmail.com>
2024-09-10 11:35:44 +0700

    Update Bento Grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 
 const Grid = () => {
-  return (
-    <div>Grid</div>
-  )
+    return (
+        <section id="about">
+            <BentoGrid>
+                {[
+                    { title: 'Title1', description: 'Desc1', id: 1 },
+                    { title: 'Title1', description: 'Desc1', id: 1 },
+                    { title: 'Title1', description: 'Desc1', id: 1 },
+                    { title: 'Title1', description: 'Desc1', id: 1 },
+                ].map((item) => (
+                    <BentoGridItem
+                        id={item.id}
+                        key={item.id}
+                        title={item.title}
+                        description={item.description}
+                    />
+                ))}
+
+            </BentoGrid>
+        </section>
+    )
 }
 
 export default Grid
